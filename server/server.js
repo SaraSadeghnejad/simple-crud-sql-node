37852d9a8250f65a4bfa0d6ec958289c037202fd
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2024-03-26 02:30:05 +0330

    final commit

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const port = 5000;
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Sara.96sd",
+  password: "",
   database: "students",
   insecureAuth: true
 });
